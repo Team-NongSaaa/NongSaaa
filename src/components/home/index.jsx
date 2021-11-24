4798e24917a2fab75c7bf582c965922e4210f8a0
LionLin <deborah91298@hotmail.com>
2021-11-24 14:10:58 +0900

    homepage

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,8 +5,64 @@ import * as S from './style';
 function Home() {
   return (
     <S.Container>
-      <Header />
-      <div>Home</div>
+      <S.HomeWrapper>
+        <S.HomeContent>
+          <S.TitleOne>TO THE NEXT LEVEL</S.TitleOne>
+          <S.TitleTwo>AI 기반 농업 데이터 분석 솔루션 Cropper</S.TitleTwo>
+          <S.BetaButton>데모 신청하기</S.BetaButton>
+        </S.HomeContent>
+      </S.HomeWrapper>
+      <S.Introduction>
+        <S.IntroOne>데이터에 기반한 과학적 농업,</S.IntroOne>
+        <S.IntroTwo>Cropper “All-in-one” 솔루션</S.IntroTwo>
+      </S.Introduction>
+      <S.BodyOne>
+        <S.BodyText>
+          <S.TextTitle>빠른 농지 데이터 수집</S.TextTitle>
+          <S.TextWord>
+            <S.Word>더 이상 일일이 농지를 점검할 필요가 없습니다</S.Word>
+            <S.Word>드론을 활용한 빠르고 손쉬운 데이터 수집</S.Word>
+            <S.Word>언제나 내가 원할 때 데이터를 수집하세요</S.Word>
+          </S.TextWord>
+        </S.BodyText>
+        <S.BodyImage src={'./img/home_body1.jpeg'} />
+      </S.BodyOne>
+      <S.BodyTwo>
+        <S.BodyImage2 src={'./img/home_body2.png'} />
+        <S.BodyText2>
+          <S.TextTitle2>농지와 작물,</S.TextTitle2>
+          <S.TextTitle> 정확한 데이터 분석</S.TextTitle>
+          <S.TextWord2>
+            <S.Word>AI가 제시하는 새로운 시대의 농업</S.Word>
+            <S.Word>전체적인 농지와 작물 각각의 상태를</S.Word>
+            <S.Word>AI가 정확하고 효율적으로 진단해줍니다</S.Word>
+          </S.TextWord2>
+        </S.BodyText2>
+      </S.BodyTwo>
+      <S.BodyOne>
+        <S.BodyText>
+          <S.TextTitle2>농작물 성장도와</S.TextTitle2>
+          <S.TextTitle>수확량 예측을 한 눈에</S.TextTitle>
+          <S.TextWord>
+            <S.Word>내 자식 같은 우리 농작물</S.Word>
+            <S.Word>현 상태는 어떻고 미래의 모습은 어떨까요?</S.Word>
+            <S.Word>데이터와 함께, 풍년을 기원해보아요</S.Word>
+          </S.TextWord>
+        </S.BodyText>
+        <S.BodyImage src={'./img/home_body3.jpeg'} />
+      </S.BodyOne>
+      <S.BodyTwo>
+        <S.BodyImage2 src={'./img/home_body2.png'} />
+        <S.BodyText2>
+          <S.TextTitle>농작업 보조 전용 플랫폼</S.TextTitle>
+          <S.TextWord2>
+            <S.Word>“All-in-one” Cropper 플랫폼에서</S.Word>
+            <S.Word>농작업의 모든 과정을 한 눈에</S.Word>
+            <S.Word>최소의 투입으로 최대의 효율을 이뤄보세요</S.Word>
+          </S.TextWord2>
+        </S.BodyText2>
+      </S.BodyTwo>
+      <S.apply></S.apply>
     </S.Container>
   );
 }
--- src/components/home/style.js
+++ src/components/home/style.js
@@ -3,3 +3,167 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
 `;
+
+export const HomeWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background-image: url('./img/homepage.jpeg');
+  background-size: cover;
+  background-position: center;
+`;
+
+export const HomeContent = styled.div`
+  height: 40%;
+  width: 100%;
+  margin: auto 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  color: white;
+`;
+
+export const BetaButton = styled.button`
+  background: #008dd4;
+  color: white;
+  border: none;
+  height: 50px;
+  width: 150px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TitleOne = styled.div`
+  font-size: 50px;
+  font-weight: 900;
+  color: black;
+`;
+
+export const TitleTwo = styled.div`
+  font-size: 23px;
+  line-height: 35px;
+  font-weight: 500;
+  color: black;
+`;
+
+export const Introduction = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: #252631;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  line-height: 35px;
+`;
+
+export const IntroOne = styled.p`
+  color: white;
+  font-weight: 500;
+  font-size: 27px;
+  margin: 0;
+`;
+
+export const IntroTwo = styled.p`
+  color: white;
+  font-size: 27px;
+  font-weight: 500;
+`;
+
+export const BodyOne = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BodyText = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  margin-right: 5%;
+`;
+
+export const TextTitle = styled.div`
+  color: #252631;
+  font-size: 40px;
+  font-weight: 700;
+  margin-bottom: 10%;
+`;
+
+export const TextWord = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+export const Word = styled.div`
+  font-size: 17px;
+  line-height: 25px;
+`;
+
+export const BodyImage = styled.img`
+  border-radius: 15px;
+  margin-right: 5%;
+  width: 50%;
+  height: 80%;
+`;
+
+export const BodyTwo = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: #d9d9dd;
+`;
+
+export const BodyText2 = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  margin-left: 5%;
+`;
+
+export const BodyImage2 = styled.img`
+  border-radius: 15px;
+  margin-left: 5%;
+  width: 50%;
+  height: 80%;
+`;
+
+export const TextTitle2 = styled.div`
+  color: #252631;
+  font-size: 40px;
+  font-weight: 700;
+  margin-bottom: 7%;
+`;
+
+export const TextWord2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const apply = styled.div`
+  width: 100%;
+  height: 800px;
+`;
